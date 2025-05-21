71c88c4aa385d6e9ac9420112579383897bfd50c
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-21 17:21:47 +0200

    feat: index-access-types

---
+++ index-access-types.ts
@@ -0,0 +1,13 @@
+type AppUser = {
+  name: string;
+  age: number;
+  permissions: {
+    id: string;
+    title: string;
+    description: string;
+  }[];
+};
+
+type Perms = AppUser["permissions"];
+
+type Perm = Perms[number];
